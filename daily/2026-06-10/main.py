ffc2b1113f72c79e4c6020d8fcab4e64201d300c
Lucas-Sturiao <dev.lucas.sturiao@gmail.com>
2026-06-11 07:04:43 -0300

    :sparkles: adição de desafio diário

---
+++ daily/2026-06-10/main.py
@@ -0,0 +1,41 @@
+estoque = [
+    {"nome": "Teclado", "quantidade": 10},
+    {"nome": "Mouse", "quantidade": 5},
+    {"nome": "Monitor", "quantidade": 3}
+]
+
+print("==== Sistema de controle de Estoque ====")
+print("1. Listar produtos")
+print("2. Adicionar produto")
+print("3. Atualizar quantidade")
+print("4. Remover produto")
+print("5. Sair")
+
+while True:
+    number = input("Escolha uma das opções: ")
+
+    if number == "1":
+        print(estoque)
+
+    elif number == "2":
+        nome = input("Digite o nome do produto: ")
+        quantidade = input("Digite a quantidade do produto: ")
+
+        novo = {"nome": nome, "quantidade": quantidade}
+
+        estoque.append(novo)
+        print(f"Produto {nome} adicionado ao estoque!")
+
+    elif number == "3":
+        
+        print("Em construção")
+
+    elif number == "4":
+        print("Em construção")
+
+    elif number == "5":
+        print("Saindo...")
+        break
+
+    else:
+        print("Opção inexistente!")
