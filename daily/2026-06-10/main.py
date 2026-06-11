959b5e9c4d24a886621049c7448e18657fc1b090
Lucas Sturião <dev.lucas.sturiao@gmail.com>
2026-06-11 13:59:17 -0300

    :sparkles: feat: add option 3

--- daily/2026-06-10/main.py
+++ daily/2026-06-10/main.py
@@ -27,7 +27,15 @@ while True:
         print(f"Produto {nome} adicionado ao estoque!")
 
     elif number == "3":
-        
+        item = input("Digite o item da lista voce deseja alterar: ")
+        for produto in estoque:
+            if item == produto["nome"]:
+                nova_quantidade = int(input("Digite a nova quantidade do produto: "))
+                produto["quantidade"] = nova_quantidade
+                print(estoque)
+                
+            else:
+                print("Produto inexistente.")
         print("Em construção")
 
     elif number == "4":
